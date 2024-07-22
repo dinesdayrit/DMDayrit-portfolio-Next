b99648b38d1cd8e4e03795d90b9bab2568061b46
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-22 15:19:43 +0800

    npm i framer-motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,26 @@
-import Image from "next/image";
-import React from "react";
-import HeroBg from "@/public/herobg.jpg";
-
 export default function Hero() {
   return (
-    <div id="home">
-      <Image
-        src={HeroBg}
-        alt="Hero image"
-        className="h-screen w-screen object-cover"
-        priority
-      />
+    <div
+      className="flex items-center bg-cover"
+      style={{
+        backgroundImage: `url('/herobg.jpg')`,
+        width: "100%",
+        height: "100vh",
+      }}
+      id="home"
+    >
+      <div className="px-16 md:px-32 max-w-7xl">
+        <h2 className="font-serif font-bold text-4xl md:text-6xl text-stone-100 lg:text-7xl">
+          Hello, <br />
+          I'm Dines!
+        </h2>
+        <p className="md:text-lg ml-3 mr-3 text-white">
+          A Web Developer{" "}
+          <span className="text-orange-500 font-bold inline-flex typing-animation">
+            <span className="typed-text">React.js | Node.js</span>
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
