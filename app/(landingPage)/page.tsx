684535a6d3c179668a268e3312230bd90ba156ca
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-22 17:19:35 +0800

    style: adjust  gap

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,14 +1,16 @@
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col justify-center items-center gap-16">
+      <div className="flex flex-col justify-center items-center">
         <Hero />
         <About />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 w-screen">
+      <h1>contact me</h1>
+    </footer>
+  );
+}
