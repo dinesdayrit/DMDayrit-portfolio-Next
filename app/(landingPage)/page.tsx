e84a264ba88b09829fd71e4d7d3d517c1f0291b3
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-23 12:15:35 +0800

    style: update bg in about section

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
       <Skills />
       <Footer />
     </main>
