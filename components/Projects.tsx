ad8b0f16603bf6fb85fbec57a3a41d02d9071893
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-23 14:19:47 +0800

    refactor: refactor card details on my my projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import { Card } from "flowbite-react";
 import Image from "next/image";
 
+const projects = [
+  {
+    title: "Tax Mapping (GIS Web Application)",
+    description:
+      "A GIS Web Application for an LGU. This application helps users locate the exact location of land properties and can also plot parcels using their land titles technical descriptions, as well as assign Property Index Numbers (PINs).",
+    image: "/gis.png",
+    technologies: ["React", "ExpressJS", "PostGreSql"],
+  },
+  {
+    title: "Delinquency APP",
+    description:
+      "A Web Application for an LGU. This application generates delinquent reports for individuals who have not paid taxes on their properties. It can also create PDF notice reports to be received by the delinquent persons.",
+    image: "/delinquent.png",
+    technologies: ["React", "ExpressJS", "PostGreSql"],
+  },
+];
+
 export default function Projects() {
   return (
     <div
@@ -12,66 +29,39 @@ export default function Projects() {
         My Projects
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-center">
-        <Card
-          className="max-w-sm shadow-lg border-sky-900 rounded-lg h-[26rem]"
-          renderImage={() => (
-            <Image width={500} height={500} src="/gis.png" alt="image 1" />
-          )}
-        >
-          <div className="flex flex-col h-full justify-between">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-300 dark:text-white text-center">
-              Tax Mapping (GIS Web Application)
-            </h5>
-            <p className="font-normal text-gray-400 dark:text-gray-400 px-2 flex-grow">
-              A GIS Web Application for an LGU. This application helps users
-              locate the exact location of land properties and can also plot
-              parcels using their land titles technical descriptions, as well as
-              assign Property Index Numbers (PINs).
-            </p>
-            <ul className="flex gap-2 text-gray-200 py-2 mx-auto">
-              <li className="border px-2 py-1 rounded-full text-sm">React</li>
-              <li className="border px-2 py-1 rounded-full text-sm">
-                ExpressJS
-              </li>
-              <li className="border px-2 py-1 rounded-full text-sm">
-                PostGreSql
-              </li>
-            </ul>
-          </div>
-        </Card>
-
-        <Card
-          className="max-w-sm shadow-lg border-sky-900 rounded-lg h-[26rem]"
-          renderImage={() => (
-            <Image
-              width={500}
-              height={500}
-              src="/delinquent.png"
-              alt="image 2"
-            />
-          )}
-        >
-          <div className="flex flex-col h-full justify-between">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-300 dark:text-white text-center">
-              Delinquency APP
-            </h5>
-            <p className="font-normal text-gray-400 dark:text-gray-400 px-2 flex-grow">
-              A Web Application for an LGU. This application generates
-              delinquent reports for individuals who have not paid taxes on
-              their properties. It can also create PDF notice reports to be
-              received by the delinquent persons.
-            </p>
-            <ul className="flex gap-2 text-gray-200 py-2 mx-auto">
-              <li className="border px-2 py-1 rounded-full text-sm">React</li>
-              <li className="border px-2 py-1 rounded-full text-sm">
-                ExpressJS
-              </li>
-              <li className="border px-2 py-1 rounded-full text-sm">
-                PostGreSql
-              </li>
-            </ul>
-          </div>
-        </Card>
+        {projects.map((project, index) => (
+          <Card
+            key={index}
+            className="max-w-sm shadow-lg border-sky-900 rounded-lg h-[26rem]"
+            renderImage={() => (
+              <Image
+                width={500}
+                height={500}
+                src={project.image}
+                alt={`image ${index + 1}`}
+              />
+            )}
+          >
+            <div className="flex flex-col h-full justify-between">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-300 dark:text-white text-center">
+                {project.title}
+              </h5>
+              <p className="font-normal text-gray-400 dark:text-gray-400 px-2 flex-grow">
+                {project.description}
+              </p>
+              <ul className="flex gap-2 text-gray-200 py-2 mx-auto">
+                {project.technologies.map((tech, techIndex) => (
+                  <li
+                    key={techIndex}
+                    className="border px-2 py-1 rounded-full text-sm"
+                  >
+                    {tech}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
