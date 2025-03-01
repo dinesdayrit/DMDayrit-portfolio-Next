9b973b0d0f1f169be1ff2d9fb28cde12106d0a95
dinesdayrit <dayritdines22@gmail.com>
2025-03-01 11:32:19 +0800

    update project section design

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -81,75 +81,74 @@ const Projects = () => {
   const [showAll, setShowAll] = useState(false);
 
   return (
-    <div
-      className="bg-sky-800 py-16 flex flex-col items-center justify-center"
-      id="projects"
-    >
-      <h1 className="font-serif font-bold text-xl md:text-3xl text-white flex flex-col md:flex-row gap-4 mb-5">
-        My Projects
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-center">
-        {projects
-          .slice(0, showAll ? projects.length : 2)
-          .map((project, index) => (
-            <motion.div
-              key={index}
-              initial="offscreen"
-              whileInView="onscreen"
-              whileHover={{ scale: 1.1 }}
-              viewport={{ once: true, amount: 0.3 }}
-              variants={cardVariants}
-              className="perspective-1000"
-            >
-              <Card
-                className="max-w-sm shadow-lg border-sky-900 rounded-lg h-[26rem] relative"
-                renderImage={() => (
-                  <Image
-                    width={500}
-                    height={500}
-                    src={project.image}
-                    alt={`image ${index + 1}`}
-                  />
-                )}
+    <div className="px-2" id="projects">
+      <div className="bg-sky-800 py-16 flex flex-col items-center justify-center rounded-xl">
+        <h1 className="font-serif font-bold text-xl md:text-3xl text-white flex flex-col md:flex-row gap-4 mb-5">
+          My Projects
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-center px-4">
+          {projects
+            .slice(0, showAll ? projects.length : 2)
+            .map((project, index) => (
+              <motion.div
+                key={index}
+                initial="offscreen"
+                whileInView="onscreen"
+                whileHover={{ scale: 1.1 }}
+                viewport={{ once: true, amount: 0.3 }}
+                variants={cardVariants}
+                className="perspective-1000"
               >
-                <div className="flex flex-col h-full justify-between">
-                  <h5 className="text-2xl font-bold tracking-tight text-gray-200 text-center">
-                    {project.title}
-                  </h5>
-                  <p className="font-normal text-gray-400 hover:text-white px-2">
-                    {project.description}
-                  </p>
-                  {project.href && (
-                    <a
-                      href={project.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 hover:text-blue-700 px-2"
-                    >
-                      Visit Site: {project.href}
-                    </a>
+                <Card
+                  className="max-w-sm shadow-lg border-sky-900 rounded-lg md:h-[26rem] relative"
+                  renderImage={() => (
+                    <Image
+                      width={500}
+                      height={500}
+                      src={project.image}
+                      alt={`image ${index + 1}`}
+                    />
                   )}
-                  <ul className="flex gap-2 text-gray-200 py-2 mx-auto">
-                    {project.technologies.map((tech, techIndex) => (
-                      <li
-                        key={techIndex}
-                        className="border px-2 py-1 rounded-full hover:text-orange-500 text-sm"
+                >
+                  <div className="flex flex-col h-full justify-between">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-200 text-center">
+                      {project.title}
+                    </h5>
+                    <p className="font-normal text-gray-400 hover:text-white px-2">
+                      {project.description}
+                    </p>
+                    {project.href && (
+                      <a
+                        href={project.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:text-blue-700 px-2"
                       >
-                        {tech}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </Card>
-            </motion.div>
-          ))}
+                        Visit Site: {project.href}
+                      </a>
+                    )}
+                    <ul className="flex gap-2 text-gray-200 py-2 mx-auto items-center">
+                      {project.technologies.map((tech, techIndex) => (
+                        <li
+                          key={techIndex}
+                          className="border px-2 py-1 rounded-full hover:text-orange-500 text-sm"
+                        >
+                          {tech}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </Card>
+              </motion.div>
+            ))}
+        </div>
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="mt-6 px-4 py-2 rounded-full border border-sky-500 shadow-lg text-white hover:bg-sky-700"
+        >
+          {showAll ? "Show Less" : "Show More"}
+        </button>
       </div>
-      <button
-        onClick={() => setShowAll(!showAll)}
-        className="mt-6 px-4 py-2 rounded-full border border-sky-500 shadow-lg text-white hover:bg-sky-700"
-      >
-        {showAll ? "Show Less" : "Show More"}
-      </button>
     </div>
   );
 };
